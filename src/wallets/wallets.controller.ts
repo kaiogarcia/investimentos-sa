8709f44effafe8cc5f3ff64d4c07c8b30326215a
kaiogarcia <kaio.vinicius_ata@hotmail.com>
2025-02-20 15:41:36 -0300

    Adiciona projeto NestJS

---
+++ src/wallets/wallets.controller.ts
@@ -0,0 +1,41 @@
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { WalletsService } from './wallets.service';
+import { CreateWalletDto } from './dto/create-wallet.dto';
+//import { WalletPresenter } from './wallet.presenter';
+
+@Controller('wallets')
+export class WalletsController {
+  constructor(private readonly walletsService: WalletsService) {}
+
+  @Post()
+  create(@Body() createWalletDto: CreateWalletDto) {
+    return this.walletsService.create(createWalletDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.walletsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.walletsService.findOne(id);
+  }
+
+  @Post(':id/assets')
+  createWalletAsset(
+    @Param('id') id: string,
+    @Body() body: { assetId: string; shares: number },
+  ) {
+    // Verificando se assetId está presente
+    if (!body.assetId || !body.shares) {
+      throw new Error('ERRO ERRADO');
+    }
+
+    return this.walletsService.createWalletAsset({
+      walletId: id,
+      assetId: body.assetId,
+      shares: body.shares,
+    });
+  }
+}
---
+++ src/wallets/wallets.service.ts
@@ -0,0 +1,73 @@
+import { Injectable } from '@nestjs/common';
+import { CreateWalletDto } from './dto/create-wallet.dto';
+import { Wallet } from './entities/wallet.entity';
+import { InjectConnection, InjectModel } from '@nestjs/mongoose';
+import mongoose, { Model } from 'mongoose';
+import { WalletAsset } from './entities/wallet-asset.entity';
+//import { UpdateWalletDto } from './dto/update-wallet.dto';
+
+@Injectable()
+export class WalletsService {
+  constructor(
+    @InjectModel(Wallet.name) private walletSchema: Model<Wallet>,
+    @InjectModel(WalletAsset.name)
+    private walletAssetSchema: Model<WalletAsset>,
+    @InjectConnection() private connection: mongoose.Connection,
+  ) {}
+
+  create(createWalletDto: CreateWalletDto) {
+    return this.walletSchema.create(createWalletDto);
+  }
+
+  findAll() {
+    return this.walletSchema.find();
+  }
+
+  findOne(id: string) {
+    return this.walletSchema.findById(id).populate([
+      {
+        path: 'assets',
+        populate: ['asset'],
+      },
+    ]);
+  }
+
+  async createWalletAsset(data: {
+    walletId: string;
+    assetId: string;
+    shares: number;
+  }) {
+    const session = await this.connection.startSession();
+    await session.startTransaction();
+    try {
+      const docs = await this.walletAssetSchema.create(
+        [
+          {
+            wallet: data.walletId,
+            asset: data.assetId,
+            shares: data.shares,
+          },
+        ],
+        { session },
+      );
+      const walletAsset = docs[0];
+      this.walletSchema.updateOne(
+        { _id: data.walletId },
+        {
+          $push: { assets: walletAsset._id },
+        },
+        {
+          session,
+        },
+      );
+      await session.commitTransaction();
+      return walletAsset;
+    } catch (e) {
+      console.error(e);
+      await session.abortTransaction();
+      throw e;
+    } finally {
+      await session.endSession();
+    }
+  }
+}
