f4b7adc55bb19c30da868a5716f993f0300db4dd
kaiogarcia <kaio.vinicius_ata@hotmail.com>
2025-02-20 22:46:18 -0300

    add frontend

--- src/app/assets/page.tsx
+++ src/app/assets/page.tsx
@@ -1,10 +1,15 @@
-import { Table, TableBody, TableHead, TableHeadCell } from "flowbite-react";
-import { WalletList } from "../components/WalletList";
-import { getMyWallet } from "../queries/queries";
-import { AssetsSync } from "../components/AssetsSync";
-import { TableWalletAssetRow } from "./TableWalletAssetRow";
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableHeadCell,
+} from "flowbite-react";
+import { WalletList } from "../../components/WalletList";
+import { getAssets, getMyWallet } from "../../queries/queries";
+import { AssetsSync } from "../../components/AssetsSync";
+import { TableAssetRow } from "./TableAssetRow";
 
-export default async function MyWalletListPage({
+export default async function AssetsListPage({
   searchParams,
 }: {
   searchParams: Promise<{ wallet_id: string }>;
@@ -21,35 +26,28 @@ export default async function MyWalletListPage({
     return <WalletList />;
   }
 
+  const assets = await getAssets();
+
   return (
     <div className="flex flex-col space-y-5 flex-grow">
       <article className="format">
-        <h1>Minha carteira</h1>
+        <h1>Ativos</h1>
       </article>
       <div className="overflow-x-auto w-full">
         <Table className="w-full max-w-full table-fixed">
           <TableHead>
             <TableHeadCell>Ativo</TableHeadCell>
             <TableHeadCell>Cotação</TableHeadCell>
-            <TableHeadCell>Quantidade</TableHeadCell>
             <TableHeadCell>Comprar/vender</TableHeadCell>
           </TableHead>
           <TableBody>
-            {wallet.assets.map((walletAsset, key) => (
-              <TableWalletAssetRow
-                key={key}
-                walletAsset={walletAsset}
-                walletId={wallet_id}
-              />
+            {assets.map((asset, key) => (
+              <TableAssetRow key={key} asset={asset} walletId={wallet_id} />
             ))}
           </TableBody>
         </Table>
       </div>
-      <AssetsSync
-        assetsSymbols={wallet.assets.map(
-          (walletAsset) => walletAsset.asset.symbol
-        )}
-      />
+      <AssetsSync assetsSymbols={assets.map((asset) => asset.symbol)} />
     </div>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import Navbar from "../components/Navbar";
+import { ToastContainer } from "../components/ToastContainer";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,10 +15,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`h-screen flex flex-col`}>
+        <Navbar />
+        <div className="container mx-auto px-4 flex flex-grow">{children}</div>
+        <ToastContainer/>
       </body>
     </html>
   );
